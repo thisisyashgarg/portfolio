0a0c8f499d78033c3a5bf2ffdbf2b59407f87602
Yash Garg <thisisyashgarg@gmail.com>
2025-12-17 23:50:42 +0530

    docs: updated projects

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -42,15 +42,15 @@ const data = {
         tenure: "Mar 2024 - Oct 2024",
         websiteLink: "https://app.dev.patent.ninja/",
       },
-      {
-        name: "Ventura Securities",
-        description: [
-          "I developed server-side mutual fund sections using React Server Components, utilizing Next.js as the primary tech stack.",
-          "This implementation optimized the rendering process, enabling efficient data fetching and improving performance for complex financial data displays."
-        ],
-        tenure: "Aug 2024 - Sept 2024",
-        websiteLink: "https://www.venturasecurities.com/",
-      },
+      // {
+      //   name: "Ventura Securities",
+      //   description: [
+      //     "I developed server-side mutual fund sections using React Server Components, utilizing Next.js as the primary tech stack.",
+      //     "This implementation optimized the rendering process, enabling efficient data fetching and improving performance for complex financial data displays."
+      //   ],
+      //   tenure: "Aug 2024 - Sept 2024",
+      //   websiteLink: "https://www.venturasecurities.com/",
+      // },
       {
         name: "Hiretal",
         description: [
@@ -73,25 +73,25 @@ const data = {
         tenure: "June 2023 - June 2024",
         websiteLink: "https://www.bluelearn.in/",
       },
-      {
-        name: "Matrimonials",
-        description: [
-          "Improved application robustness by solving multiple bugs, added DOM to PDF generation, and integrated search filters, utilizing React.js, Node.js, EC2, and MongoDB"
-        ],
-        tenure: "June 2023 - July 2023 (Freelance - Full Stack Developer)",
-        websiteLink: "https://www.matrimonials.ai/",
-      },
-      {
-        name: "Intelligent Labs",
-        description: [
-          "Developed a company collaboration platform from scratch, working closely with the founders to brainstorm the app flow.",
-          "Took responsibility for the entire user journey, including user onboarding, authentication, search, feed, and profile.",
-          "Implemented a Node.js backend using the MVC architecture, incorporating JWT authentication.",
-          "Successfully built a dynamic feed page and search functionality that updates in real-time with new posts."
-        ],
-        tenure: "April 2023 (Full Stack Developer Intern)",
-        websiteLink: "https://reacti.ai/",
-      },
+      // {
+      //   name: "Matrimonials",
+      //   description: [
+      //     "Improved application robustness by solving multiple bugs, added DOM to PDF generation, and integrated search filters, utilizing React.js, Node.js, EC2, and MongoDB"
+      //   ],
+      //   tenure: "June 2023 - July 2023 (Freelance - Full Stack Developer)",
+      //   websiteLink: "https://www.matrimonials.ai/",
+      // },
+      // {
+      //   name: "Intelligent Labs",
+      //   description: [
+      //     "Developed a company collaboration platform from scratch, working closely with the founders to brainstorm the app flow.",
+      //     "Took responsibility for the entire user journey, including user onboarding, authentication, search, feed, and profile.",
+      //     "Implemented a Node.js backend using the MVC architecture, incorporating JWT authentication.",
+      //     "Successfully built a dynamic feed page and search functionality that updates in real-time with new posts."
+      //   ],
+      //   tenure: "April 2023 (Full Stack Developer Intern)",
+      //   websiteLink: "https://reacti.ai/",
+      // },
       {
         name: "Slingshot",
         description: [
@@ -103,9 +103,9 @@ const data = {
     ],
   },
   skills: [
-    {
-      skillName: "React Native",
-    },
+    // {
+    //   skillName: "React Native",
+    // },
     {
       skillName: "PostgreSQL"
     },
@@ -157,50 +157,50 @@ const data = {
         "https://res.cloudinary.com/dwwtffefs/image/upload/v1728134691/Screenshot_2024-10-05_at_6.51.44_PM_iek2ah.png",
       ],
     },
-    {
-      title: "UnQueue",
-      description:
-        "UnQueue eliminates physical queues by joining virtual queues from your device, receive turn notifications, making it convenient and efficient.",
-      tags: ["react", "tailwindcss", "react-router-dom", "firebase"],
-      codeLink: "https://github.com/thisisyashgarg/unqueue",
-      websiteLink: "https://unqueue.netlify.app/",
-      androidAppLink:
-        "https://drive.google.com/file/d/1jlq_eQv7EnLtAJlUlP_37OnmFaApuuPT/view?usp=drivesdk",
-      screenshots: [
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677179980/unqueue-ss/homepage_bej3xk.png",
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677179980/unqueue-ss/domains_d11nth.png",
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677179979/unqueue-ss/dashboard_gepykw.png",
-      ],
-    },
-    {
-      title: "Foodify",
-      description: "Foodify is a food exploration app",
-      tags: ["react", "tailwindcss", "redux-toolkit", "typescript"],
-      codeLink: "https://github.com/thisisyashgarg/namaste-react",
-      websiteLink: "https://foodifyme.netlify.app/",
-      screenshots: [
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677495619/foodify/home_rdikc3.png",
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677495618/foodify/menu_ibpjha.png",
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677495618/foodify/cart_lio0de.png",
-      ],
-    },
-    {
-      title: "Ain't Youtube",
-      description: "Ain't Youtube is a video streaming app",
-      tags: [
-        "react",
-        "tailwindcss",
-        "redux-toolkit",
-        "typescript",
-        "youtube-api",
-      ],
-      codeLink: "https://github.com/thisisyashgarg/youtube-clone",
-      websiteLink: "https://aintyoutube.netlify.app/",
-      screenshots: [
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1680374548/youtube-clone/Screenshot_Capture_-_2023-03-17_-_23-14-12_qf9qqa.png",
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1680374550/youtube-clone/Screenshot_Capture_-_2023-03-17_-_23-14-41_jqa6fd.png",
-      ],
-    },
+    // {
+    //   title: "UnQueue",
+    //   description:
+    //     "UnQueue eliminates physical queues by joining virtual queues from your device, receive turn notifications, making it convenient and efficient.",
+    //   tags: ["react", "tailwindcss", "react-router-dom", "firebase"],
+    //   codeLink: "https://github.com/thisisyashgarg/unqueue",
+    //   websiteLink: "https://unqueue.netlify.app/",
+    //   androidAppLink:
+    //     "https://drive.google.com/file/d/1jlq_eQv7EnLtAJlUlP_37OnmFaApuuPT/view?usp=drivesdk",
+    //   screenshots: [
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677179980/unqueue-ss/homepage_bej3xk.png",
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677179980/unqueue-ss/domains_d11nth.png",
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677179979/unqueue-ss/dashboard_gepykw.png",
+    //   ],
+    // },
+    // {
+    //   title: "Foodify",
+    //   description: "Foodify is a food exploration app",
+    //   tags: ["react", "tailwindcss", "redux-toolkit", "typescript"],
+    //   codeLink: "https://github.com/thisisyashgarg/namaste-react",
+    //   websiteLink: "https://foodifyme.netlify.app/",
+    //   screenshots: [
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677495619/foodify/home_rdikc3.png",
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677495618/foodify/menu_ibpjha.png",
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677495618/foodify/cart_lio0de.png",
+    //   ],
+    // },
+    // {
+    //   title: "Ain't Youtube",
+    //   description: "Ain't Youtube is a video streaming app",
+    //   tags: [
+    //     "react",
+    //     "tailwindcss",
+    //     "redux-toolkit",
+    //     "typescript",
+    //     "youtube-api",
+    //   ],
+    //   codeLink: "https://github.com/thisisyashgarg/youtube-clone",
+    //   websiteLink: "https://aintyoutube.netlify.app/",
+    //   screenshots: [
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1680374548/youtube-clone/Screenshot_Capture_-_2023-03-17_-_23-14-12_qf9qqa.png",
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1680374550/youtube-clone/Screenshot_Capture_-_2023-03-17_-_23-14-41_jqa6fd.png",
+    //   ],
+    // },
     {
       title: "Cryptologer",
       description:
@@ -213,16 +213,16 @@ const data = {
         "https://res.cloudinary.com/dwwtffefs/image/upload/v1677303411/news-bot/Screenshot_Capture_-_2023-02-25_-_11-06-38_kvcpnu.png",
       ],
     },
-    {
-      title: "Book Management API ( with Swagger Docs)",
-      description: "A simple API for book management",
-      tags: ["nodejs", "typescript", "swagger", "docker", "mvc"],
-      codeLink: "https://github.com/thisisyashgarg/vocally-assignment",
-      websiteLink: "",
-      screenshots: [
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1680375357/books-api/Screenshot_Capture_-_2023-04-02_-_00-25-09_xnsdo8.png",
-      ],
-    },
+    // {
+    //   title: "Book Management API ( with Swagger Docs)",
+    //   description: "A simple API for book management",
+    //   tags: ["nodejs", "typescript", "swagger", "docker", "mvc"],
+    //   codeLink: "https://github.com/thisisyashgarg/vocally-assignment",
+    //   websiteLink: "",
+    //   screenshots: [
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1680375357/books-api/Screenshot_Capture_-_2023-04-02_-_00-25-09_xnsdo8.png",
+    //   ],
+    // },
     {
       title: "Meme Generator",
       description:
@@ -234,34 +234,34 @@ const data = {
         "https://res.cloudinary.com/dwwtffefs/image/upload/v1677136172/meme-generator/Screenshot_Capture_-_2023-02-23_-_12-38-21_tmrqke.png",
       ],
     },
-    {
-      title: "FooBank - Modern Banking Website",
-      description:
-        "Completely responsive modern banking website made out of just two dependencies, React and Tailwiind",
-      tags: ["react", "typescript", "tailwindcss"],
-      codeLink: "https://github.com/thisisyashgarg/banking-app",
-      websiteLink: "https://foobank.netlify.app/",
-      screenshots: [
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761304/foobank/Screenshot_Capture_-_2023-03-02_-_18-16-08_awcva1.png",
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761304/foobank/Screenshot_Capture_-_2023-03-02_-_18-16-32_gtfpak.png",
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761304/foobank/Screenshot_Capture_-_2023-03-02_-_18-16-58_mpxjpc.png",
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761303/foobank/Screenshot_Capture_-_2023-03-02_-_18-17-17_vjhpda.png",
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761303/foobank/Screenshot_Capture_-_2023-03-02_-_18-16-26_xytphk.png",
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761303/foobank/Screenshot_Capture_-_2023-03-02_-_18-17-21_iwnxkx.png",
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761303/foobank/Screenshot_Capture_-_2023-03-02_-_18-16-15_ik4d4z.png",
-      ],
-    },
-    {
-      title: "Tenzies Game",
-      description:
-        "Tenzies is a React-based dice game in which the goal is to roll until all dice are the same.",
-      tags: ["react", "typescript", "tailwindcss"],
-      codeLink: "https://github.com/thisisyashgarg/tenzies-game",
-      websiteLink: "https://tenziesmatch.netlify.app/",
-      screenshots: [
-        "https://res.cloudinary.com/dwwtffefs/image/upload/v1679938405/tenzies/Screenshot_Capture_-_2023-03-27_-_23-03-08_putehk.png",
-      ],
-    },
+    // {
+    //   title: "FooBank - Modern Banking Website",
+    //   description:
+    //     "Completely responsive modern banking website made out of just two dependencies, React and Tailwiind",
+    //   tags: ["react", "typescript", "tailwindcss"],
+    //   codeLink: "https://github.com/thisisyashgarg/banking-app",
+    //   websiteLink: "https://foobank.netlify.app/",
+    //   screenshots: [
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761304/foobank/Screenshot_Capture_-_2023-03-02_-_18-16-08_awcva1.png",
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761304/foobank/Screenshot_Capture_-_2023-03-02_-_18-16-32_gtfpak.png",
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761304/foobank/Screenshot_Capture_-_2023-03-02_-_18-16-58_mpxjpc.png",
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761303/foobank/Screenshot_Capture_-_2023-03-02_-_18-17-17_vjhpda.png",
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761303/foobank/Screenshot_Capture_-_2023-03-02_-_18-16-26_xytphk.png",
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761303/foobank/Screenshot_Capture_-_2023-03-02_-_18-17-21_iwnxkx.png",
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1677761303/foobank/Screenshot_Capture_-_2023-03-02_-_18-16-15_ik4d4z.png",
+    //   ],
+    // },
+    // {
+    //   title: "Tenzies Game",
+    //   description:
+    //     "Tenzies is a React-based dice game in which the goal is to roll until all dice are the same.",
+    //   tags: ["react", "typescript", "tailwindcss"],
+    //   codeLink: "https://github.com/thisisyashgarg/tenzies-game",
+    //   websiteLink: "https://tenziesmatch.netlify.app/",
+    //   screenshots: [
+    //     "https://res.cloudinary.com/dwwtffefs/image/upload/v1679938405/tenzies/Screenshot_Capture_-_2023-03-27_-_23-03-08_putehk.png",
+    //   ],
+    // },
     {
       title: "Discord Tweeter Bot",
       description:

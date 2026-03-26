a9775f3e35e19328a7d612bdd1f5e4648d2c0548
Yash Garg <thisisyashgarg@gmail.com>
2026-03-26 17:08:06 +0530

    docs: udpated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+const isDev = process.env.NODE_ENV === "development";
+
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    isDev && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
